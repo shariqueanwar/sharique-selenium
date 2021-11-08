88fe712afabf1c12bd42cd04f9fa020ec6e9359a
shariqueanwar <anwarsharique28@gmail.com>
2021-11-08 23:04:08 +0530

    first

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+public class GreensTech {
+
+	private void greensomr() {
+		System.out.println("thuraipakkam");
+		
+	}
+	private void greensAdyar() {
+		System.out.println("adyar");
+
+	}
+	private void greensTambaram() {
+		System.out.println("tambaram");
+
+	}
+	private void greensVelachery() {
+		System.out.println("velachery");
+		
+	}
+	private void greensAnnanagar() {
+		System.out.println("anna nagar");
+
+	}
+	public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.greensAdyar();
+		g.greensTambaram();
+		g.greensAnnanagar();
+		g.greensomr();
+		g.greensVelachery();
+	}
+}
